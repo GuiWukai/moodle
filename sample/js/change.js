e1f5e2c32613198e87b6b1ce1d6bd560777b7479
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2017-06-09 18:18:38 -0400

    hello

---
+++ sample/js/change.js
@@ -0,0 +1,14 @@
+function changeToMaterialize()
+{
+	var element = document.getElementByClassName("framework");
+
+	element.href = "css/materialize.min.css";
+}
+
+function changeToBootstrap()
+{
+	var element = document.getElementByClassName("framework");
+
+	element.href = "css/bootstrap.min.css";
+}
+
